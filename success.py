0d6afbdd832c80c83acf8c569b21d06c305d106b
KapsonLabs <kapson65536@gmail.com>
2017-08-03 12:28:17 +0300

    Add files via upload

---
+++ success.py
@@ -0,0 +1,5 @@
+import tensorflow as tf 
+
+print("Your tensorflow version is", tf.__version__)
+print("")
+print("You have successfully installed tensorflow, close window to return to main menu")
---
+++ version.py
@@ -0,0 +1,10 @@
+import tensorflow as tf 
+import cv2
+
+print("Your tensorflow version is", tf.__version__)
+print("")
+print("Your Open Cv version is", cv2.__version__)
+print("")
+print("YOU HAVE SUCCESSFULLY INSTALLED TENSORFLOW AND OPEN CV. WE ARE ALMOST DONE")
+print("")
+print("CLOSE WINDOW TO GO BACK TO MAIN WINDOW")
